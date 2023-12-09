b9101bf5bdf858fff7e1aed8e0426287ed2508fa
violetta <lotsmanovavioletta@gmail.com>
2023-12-09 10:37:17 +0300

    add view, urls and serializers

---
+++ trading/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from trading.apps import TradingConfig
+from trading.views import NetworkNodeViewSet
+
+app_name = TradingConfig.name
+router = DefaultRouter()
+router.register(r'trading', NetworkNodeViewSet, basename='trading')
+
+urlpatterns = router.urls
--- trading/views.py
+++ trading/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from trading.models import NetworkNode
+from trading.serializers import TradingSerializer, TradingUpdateSerializer
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    queryset = NetworkNode.objects.all()
+
+    default_serializer = TradingSerializer
+    serializers = {
+        'update': TradingUpdateSerializer,
+        'partial_update': TradingUpdateSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.default_serializer)
